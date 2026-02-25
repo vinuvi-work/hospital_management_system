5990aac97e8b2be4e7725ce4f9d31408e1093d27
vinuvi-work <vinuvi@dynamic-labs.ai>
2026-02-25 12:28:38 +0530

    create patient dummy datas

--- hospital-app/database/seeders/DatabaseSeeder.php
+++ hospital-app/database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Doctor;
+use App\Models\Patient;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,6 +18,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         Doctor::factory(10)->create();
+        Patient::factory(10)->create();
 
         User::factory()->create([
             'name' => 'admin user',
