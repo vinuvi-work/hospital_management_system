50f679788ee61bd9babadcfcd85f68e41b6370b1
vinuvi-work <vinuvi@dynamic-labs.ai>
2026-02-25 10:24:26 +0530

    create an admin user and dummy data

--- hospital-app/database/seeders/DatabaseSeeder.php
+++ hospital-app/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Doctor;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Doctor::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'admin user',
+            'email' => 'admin@gmail.com',
+            'password' => '1234'
         ]);
     }
 }
